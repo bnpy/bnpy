079bb68416c3641aef10fe5db7ec21793a1e90be
Gabriel Hope <john_hope@brown.edu>
2017-05-09 19:21:22 -0400

    Added much faster cython implementation of q(z) update for sLDA

---
+++ bnpy/allocmodel/topics/SupervisedHelper.pyx
@@ -0,0 +1,34 @@
+import numpy as np
+from libc.math cimport exp
+
+def calcRespInner_cython(double[:, :] resp, double[:] Zbar, double[:] wc_d, double[:, :] E_outer, double l_div_Nd_2):
+    cdef int N = resp.shape[0]
+    cdef int K = resp.shape[1]
+
+    cdef double update = 0.0
+    cdef double respsum = 0.0
+
+    #Loop over tokens
+    for i in range(N):
+        
+        #Subtract current token from Zbar
+        for k in range(K):
+            Zbar[k] -= wc_d[i] * resp[i, k]
+
+        #Compute the update to the resp for token i
+        respsum = 0.0
+        for k in range(K):
+            update = E_outer[k, k] 
+            for k2 in range(K):
+                update += 2 * Zbar[k2] * E_outer[k2, k]
+
+            resp[i, k] *= exp(- l_div_Nd_2 * update)
+            respsum += resp[i, k]
+
+        #Normalize & update Zbar with the new resp
+        for k in range(K):
+            resp[i, k] /= respsum
+            Zbar[k] += wc_d[i] * resp[i, k]
+            
+    return resp, Zbar
+
