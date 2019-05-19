c20eb1b65e4d55643c69c79521068eea7cacf2e8
Geng Ji <gengji@Gengs-MacBook-Pro.local>
2019-05-19 16:02:34 -0700

    ADD a test case for FwdAlg_sparse_cpp

--- bnpy/allocmodel/hmm/lib/test_LibFwdBwd.py
+++ bnpy/allocmodel/hmm/lib/test_LibFwdBwd.py
@@ -27,19 +27,28 @@ if __name__ == '__main__':
 
     fmsg_TK, marglik_T = bnpy.allocmodel.hmm.lib.LibFwdBwd.FwdAlg_cpp(start_pi_K, trans_pi_KK, lik_TK)
     sp_fmsg_TL, sp_col_TL, sp_marglik_T = bnpy.allocmodel.hmm.lib.LibFwdBwd.FwdAlg_onepass_cpp(start_pi_K, trans_pi_KK, lik_TK, L)
+    sp_fmsg_TL_2, sp_col_TL_2, sp_marglik_T_2 = bnpy.allocmodel.hmm.lib.LibFwdBwd.FwdAlg_sparse_cpp(start_pi_K, trans_pi_KK, lik_TK, L)
 
     sp_fmsg_TK = np.zeros((T,K))
+    sp_fmsg_TK_2 = np.zeros((T,K))
     for t in range(T):
         for m in range(L):
             sp_fmsg_TK[t, sp_col_TL[t,m]] = sp_fmsg_TL[t,m]
+            sp_fmsg_TK_2[t, sp_col_TL_2[t,m]] = sp_fmsg_TL_2[t,m]
         print("DENSE  fmsg_TK[t=%d]: %s" % (t, fmsg_TK[t]))
-        print("SPARSE fmsg_TK[t=%d]: %s" % (t, sp_fmsg_TK[t]))
+        print("ONEPASS fmsg_TK[t=%d]: %s" % (t, sp_fmsg_TK[t]))
+        print("O(L^2) fmsg_TK[t=%d]: %s" % (t, sp_fmsg_TK_2[t]))
+        print
 
 
     print("DENSE marglik_T:")
     print(marglik_T)
-    print("SPARSE marglik_T:")
+    print("ONEPASS marglik_T:")
     print(sp_marglik_T)
+    print("O(L^2) marglik_T:")
+    print(sp_marglik_T_2)
     if L == K:
         assert np.allclose(sp_marglik_T, marglik_T)
         assert np.allclose(sp_fmsg_TK, fmsg_TK)
+        assert np.allclose(sp_marglik_T_2, marglik_T)
+        assert np.allclose(sp_fmsg_TK_2, fmsg_TK)
