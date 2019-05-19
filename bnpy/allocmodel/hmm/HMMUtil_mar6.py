bb9e8b9d6e038e9e3ab8d30646e18d21a970c6fb
Geng Ji <gengji@Gengs-MacBook-Pro.local>
2019-05-19 14:42:49 -0700

    FIX useL2LP

--- bnpy/allocmodel/hmm/HMMUtil_mar6.py
+++ bnpy/allocmodel/hmm/HMMUtil_mar6.py
@@ -361,7 +361,7 @@ def FwdAlg(PiInit, PiMat, SoftEv, nnzPerRowLP=0, useL2=1):
             print 'sparse fwd alg'
             return FwdAlg_sparse_cpp(PiInit, PiMat, SoftEv, nnzPerRowLP)
     else:
-        return FwdAlg_py(PiInit, PiMat, SoftEv, nnzPerRowLP, useL2=useL2)
+        return FwdAlg_py(PiInit, PiMat, SoftEv, nnzPerRowLP, useL2LP=useL2)
 
 
 def BwdAlg(PiInit, PiMat, SoftEv, margPrObs=None, top_colids=None):
