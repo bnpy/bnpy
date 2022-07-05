a86bfeb59f276e47a5d783090046fd2982feabb1
Malinda <malinda.dilhara@gmail.com>
2022-07-05 00:49:05 -0700

    use multi_dot

--- bnpy/obsmodel/GaussRegressYFromFixedXObsModel.py
+++ bnpy/obsmodel/GaussRegressYFromFixedXObsModel.py
@@ -489,7 +489,7 @@ def calcPostParamsFromSSForComp(
     ptau_K = np.zeros(K)
     ptau_K[:] = SS_yy_K + Prior.ptau + Prior.wPw_1
     for k in range(K):
-        ptau_K[k] -= np.dot(w_KE[k], np.dot(P_KEE[k], w_KE[k]))
+        ptau_K[k] -= np.linalg.multi_dot([w_KE[k], P_KEE[k], w_KE[k]])
 
     return pnu_K, ptau_K, w_KE, P_KEE
 
